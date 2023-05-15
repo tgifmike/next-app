5fb44da18e07bd51186d93b8e87fc10d29d433a7
mike sorrentino <mr.mikesorrentino@gmail.com>
2023-05-15 14:43:41 -0400

    new

---
+++ sanity.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from "sanity";
+import { deskTool } from "sanity/desk";
+// import project from "./sanity/schemas/project-schema";
+import schema from "./sanity/schemas";
+import schemas from "./sanity/schemas";
+
+const config = defineConfig({
+  projectId: "lj42884w",
+  dataset: "production",
+  title: "My Personal Website",
+  apiVersion: "2023-05-08",
+  basePath: "/admin",
+  plugins: [deskTool()],
+  schema: { types: schemas },
+});
+
+export default config;
---
+++ sanity/config/client-config.ts
@@ -0,0 +1,7 @@
+const config = {
+  projectId: "lj42884w",
+  dataset: "production",
+  apiVersion: "2023-05-08",
+};
+
+export default config;
